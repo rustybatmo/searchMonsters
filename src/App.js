9c79af6769859f97d0f50351bf7cf05325d8ef21
harishb <rustybatmo1@gmail.com>
2020-12-30 17:01:36 +0530

    Included state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+        { name: "Harish", id: "1" },
+        { name: "Abilash", id: "2" },
+        { name: "Raghul", id: "3" },
+      ],
+    };
+  }
+
+  handleClick = (e) => {
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+        <button onClick={this.handleClick}>Check</button>
+      </div>
+    );
+  }
 }
 
 export default App;
